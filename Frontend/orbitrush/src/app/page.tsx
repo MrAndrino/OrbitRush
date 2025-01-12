2cbcf7bfa6abab3ca5032e2e1bedfdaa2b6f7697
MrAndrino <d.andfer96@gmail.com>
2025-01-12 14:54:30 +0100

    Creado componente línea

--- Frontend/orbitrush/src/app/page.tsx
+++ Frontend/orbitrush/src/app/page.tsx
@@ -1,26 +1,25 @@
 import Link from 'next/link';
 import Button from '@/components/button/button';
+import Line from '@/components/line/line';
 
 export default function homePage() {
   return (
     <>
       <div className="text-center">
-        <div className="relative bg-[url('/images/OrbitRush-Logo.png')] bg-cover bg-center h-64 w-full">
-          <div className="absolute inset-0 bg-black/40 backdrop-blur-md flex items-center justify-center font-primary text-7xl" >
+        <div className="relative bg-[url('/images/OrbitRush-Logo.png')] bg-cover bg-center aspect-[8/1] w-full select-none">
+          <div className="absolute inset-0 bg-black/40 backdrop-blur-md flex items-center justify-center font-primary text-2xl sm:text-4xl md:text-5xl lg:text-6xl">
             Tu nueva forma de jugar Orbito
           </div>
         </div>
-        <div>
-          <Link href="/login">
-            <Button
-              variant={"short"}
-              color={""}
-            > Joder</Button>
-          </Link>
-        </div>
-        <div>
-          Reglas
-        </div>
+
+        <Line />
+
+        <Link href="/login">
+          <Button variant="short" color="">¡Juega Ya!</Button>
+        </Link>
+    
+
+        <div className="">Reglas</div>
       </div>
     </>
   );
--- Frontend/orbitrush/src/path.ts
+++ Frontend/orbitrush/src/path.ts
@@ -1,7 +1,7 @@
 export const homePage = () => "/";
 
-//export const signUpPath = () => "/sign-up";
-//export const signInPath = () => "/sign-in";
-//export const passwordForgotPath = () => "/password-forgot";
+export const signUpPath = () => "/sign-up";
+export const signInPath = () => "/sign-in";
+export const passwordForgotPath = () => "/password-forgot";
 
 // export const menuPage = () => "/menu";
