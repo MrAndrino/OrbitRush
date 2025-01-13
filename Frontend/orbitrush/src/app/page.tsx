b726f7abf672abd93d905508ab2937e6f560633c
MrAndrino <d.andfer96@gmail.com>
2025-01-13 13:35:46 +0100

    Ajustes botonera

--- Frontend/orbitrush/src/app/page.tsx
+++ Frontend/orbitrush/src/app/page.tsx
@@ -3,13 +3,13 @@ import Line from '@/components/line/line';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col gap-10 text-center">
+    <div className="flex flex-col gap-[10vh] text-center">
       
       {/* Encabezado */}
       <div className="relative bg-[url('/images/OrbitRush-Fondo.png')] bg-cover bg-center">
-        <div className="aspect-[8/1] select-none">
-          <div className="absolute inset-0 bg-black/40 backdrop-blur-md flex items-center justify-center font-primary text-2xl sm:text-4xl md:text-5xl lg:text-6xl">
-            Orbit Rush! Tu nueva forma de jugar Órbito
+        <div className="h-[40vh] select-none">
+          <div className="absolute inset-0 bg-black/55 backdrop-blur-md flex items-center justify-center font-primary text-2xl sm:text-4xl md:text-5xl lg:text-7xl">
+            Orbit Rush!<br/>Tu nueva forma de jugar Órbito
           </div>
         </div>
         <Line />
@@ -17,15 +17,15 @@ export default function HomePage() {
 
       <div className='w-5/6 text-xl mx-auto'>Órbito es un emocionante juego de mesa tridimensional que desafía la lógica y la visión espacial de los jugadores. Con un diseño innovador y bolas de colores vibrantes, ofrece una experiencia única que combina estrategia y diversión. Perfecto para todas las edades, Órbito garantiza momentos de competencia amigable mientras pone a prueba tu capacidad para anticiparte y planificar en múltiples dimensiones.</div>
 
-      {/* Botón para jugar */}
-      <div className="flex justify-center">
+      {/* Botonera */}
+      <div className="flex justify-center gap-[5rem] select-none">
         <Button href="/login" color="blue" className="w-[15rem] h-[5rem] text-3xl">
           ¡Juega Ahora!
         </Button>
+        <Button href="" color="orange" className="w-[15rem] h-[5rem] text-3xl">
+          Cómo Jugar
+        </Button>
       </div>
-
-      {/* Sección de reglas */}
-      <div className="text-lg">Reglas</div>
     </div>
   );
 }
