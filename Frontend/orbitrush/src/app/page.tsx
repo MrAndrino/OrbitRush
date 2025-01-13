e3fed7c7737eb5e2e8232fa79615f23790a53a6b
MrAndrino <d.andfer96@gmail.com>
2025-01-13 12:57:27 +0100

    Button, Description, Title, Logo
    Terminado el botón reutilizable, añadida una descripción del juego y ajustado el título y Logo en la pestaña del navegador

--- Frontend/orbitrush/src/app/page.tsx
+++ Frontend/orbitrush/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Button from '@/components/button/button';
 import Line from '@/components/line/line';
 
@@ -7,19 +6,23 @@ export default function HomePage() {
     <div className="flex flex-col gap-10 text-center">
       
       {/* Encabezado */}
-      <div className="relative bg-[url('/images/OrbitRush-Logo.png')] bg-cover bg-center">
-        <div className="aspect-[8/1]">
+      <div className="relative bg-[url('/images/OrbitRush-Fondo.png')] bg-cover bg-center">
+        <div className="aspect-[8/1] select-none">
           <div className="absolute inset-0 bg-black/40 backdrop-blur-md flex items-center justify-center font-primary text-2xl sm:text-4xl md:text-5xl lg:text-6xl">
-            Tu nueva forma de jugar Orbito
+            Orbit Rush! Tu nueva forma de jugar Órbito
           </div>
         </div>
         <Line />
       </div>
 
+      <div className='w-5/6 text-xl mx-auto'>Órbito es un emocionante juego de mesa tridimensional que desafía la lógica y la visión espacial de los jugadores. Con un diseño innovador y bolas de colores vibrantes, ofrece una experiencia única que combina estrategia y diversión. Perfecto para todas las edades, Órbito garantiza momentos de competencia amigable mientras pone a prueba tu capacidad para anticiparte y planificar en múltiples dimensiones.</div>
+
       {/* Botón para jugar */}
-      <Link href="/login">
-        <Button color="blue">¡Juega Ahora!</Button>
-      </Link>
+      <div className="flex justify-center">
+        <Button href="/login" color="blue" className="w-[15rem] h-[5rem] text-3xl">
+          ¡Juega Ahora!
+        </Button>
+      </div>
 
       {/* Sección de reglas */}
       <div className="text-lg">Reglas</div>
--- Frontend/orbitrush/src/components/button/button.tsx
+++ Frontend/orbitrush/src/components/button/button.tsx
@@ -1,16 +1,25 @@
 import styles from '@/components/button/button.module.css'
+import Link from 'next/link';
 
 type ButtonProps = {
   children: React.ReactNode;
-  color: 'blue' | 'red' | 'green';
+  color: 'blue' | 'orange';
+  href?: string;
+  className?: string;
 };
 
-function Button({ children, color }: ButtonProps) {
-  return (
-    <div className={styles[color]}>
-      <button>{children}</button>
-    </div>
+function Button({ children, color, href, className = '' }: ButtonProps) {
+  const buttonContent = (
+    <button className={`${styles[color]} ${className}`}>
+      {children}
+    </button>
   )
+
+  if (href) {
+    return <Link href={href} >{buttonContent}</Link>;
+  }
+
+  return buttonContent;
 }
 
 export default Button;
