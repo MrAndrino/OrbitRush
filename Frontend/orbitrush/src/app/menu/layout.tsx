456aee6947ccd0f1e5e679d7191c3ba73a5fc2c0
MrAndrino <d.andfer96@gmail.com>
2025-02-02 14:58:02 +0100

    [Front] Creado Desplegable UserBox

--- Frontend/orbitrush/src/app/menu/layout.tsx
+++ Frontend/orbitrush/src/app/menu/layout.tsx
@@ -2,8 +2,8 @@
 
 import Footer from '@/components/footer/footer';
 import Header from '@/components/header/header';
-import { User } from '@/components/cards/friendcard';
-import Friendbox from '@/components/friendbox/friendbox';
+import Friendbox from '@/components/boxfriend/friendbox';
+import UserBox from '@/components/boxuser/userbox';
 
 export default function MenuLayout({
   children,
@@ -13,17 +13,17 @@ export default function MenuLayout({
 
   return (
     <section className="min-h-[100vh] flex flex-col">
-
       <Header />
 
       <section className='flex'>
-        <div className="w-[80%] h-[75vh] py-12 px-24 overflow-auto scrollbar-hidden text-center">
+        <div className="w-[83%] h-[81vh] py-12 px-24 overflow-auto scrollbar-hidden text-center">
           {children}
         </div>
 
-        <div className='w-[20%]'>
+        <div className='w-[17%]'>
           <Friendbox/>
         </div>
+        <UserBox/>
       </section>
 
       <Footer />
